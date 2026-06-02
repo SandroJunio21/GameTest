98752b977e2113aced1cb942efa12ba1ce885318
Sandro Junio <sandrojuniofm@gmail.com>
2026-06-02 10:00:48 -0300

    creating mediator pattern Enemies destruction

--- code/level.py
+++ code/level.py
@@ -6,6 +6,7 @@ import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
+from code.entityMediator import EntityMediator
 from code.Const import C_WHITE, WIN_WIDTH, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.entity import Entity
 from code.entityFactory import EntityFactory
@@ -44,15 +45,17 @@ class Level:
 
 
             self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000 :.1f}s', C_WHITE, (10,5))
-            self.level_text(14, f'fps:{clock.get_fps() :.0f}', C_WHITE, (10,WIN_WIDTH - 35))
-            self.level_text(14, f'entidades:{len(self.entity_list)}', C_WHITE, (10,WIN_WIDTH - 20) )
+            self.level_text(14, f'fps: {clock.get_fps() :.0f}', C_WHITE, (10,WIN_WIDTH - 35))
+            self.level_text(14, f'entidades: {len(self.entity_list)}', C_WHITE, (10,WIN_WIDTH - 20))
             pygame.display.flip()
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
         pass
 
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
-        text_rect: Rect = text_surf.get_rect(center=text_pos)
+        text_rect: Rect = text_surf.get_rect(left= text_pos[0], top=text_pos[1])
         self.window.blit(source=text_surf, dest=text_rect)
 
